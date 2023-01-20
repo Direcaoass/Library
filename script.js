e260d6547cea542ee74c65a9a9ff67615be9641d
Vitor <direcaoass@gmail.com>
2023-01-20 12:08:21 -0300

    basic html and css added

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert();
+let myLibrary = [];
+
+const libraryGrid=document.querySelector('.libraryGrid');
+const addBtn= document.querySelector('.addBtn');
+
+const bookTitle = document.querySelector('#titleInput');
+const bookAuthor = document.querySelector('#authorInput');
+const bookPages = document.querySelector('#pagesInput');
+const bookRead = document.querySelector('#readInput');
+
+
+function Book(title, author, pages, read, deleted) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.deleted=deleted;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function showBooks(){
+    for(let i of myLibrary){
+        
+    }
+}
+
+const newBook =  new Book(bookTitle);
+addBookToLibrary(newBook);
+addBookToLibrary(newBook)
+
+
+console.log(myLibrary);
