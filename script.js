fd5fc1a8d6b02ba85b4bcb1f5a0b3791f18b9def
Vitor <direcaoass@gmail.com>
2023-01-21 12:24:50 -0300

    book content added

--- script.js
+++ script.js
@@ -1,35 +1,102 @@
 let myLibrary = [];
 
-const libraryGrid=document.querySelector('.libraryGrid');
-const addBtn= document.querySelector('.addBtn');
+const libraryGrid = document.querySelector('.libraryGrid');
+const addBtn = document.querySelector('.addBtn');
+const popUpGrid = document.querySelector('.popUp');
 
-const bookTitle = document.querySelector('#titleInput');
-const bookAuthor = document.querySelector('#authorInput');
-const bookPages = document.querySelector('#pagesInput');
-const bookRead = document.querySelector('#readInput');
+const bookTitle = document.querySelector('.name');
+const bookAuthor = document.querySelector('.author');
+const bookPages = document.querySelector('.pages');
+const bookRead = document.querySelector('.readed');
+const submitBtn = document.querySelector('.submit');
+const canceltBtn = document.querySelector('.cancel');
+const removeBtn=document.querySelector('.removeBtn');
+const readBtn=document.querySelector('readBtn');
 
 
-function Book(title, author, pages, read, deleted) {
+let readed;
+
+//events
+
+addBtn.addEventListener('click', e => {
+    popUpGrid.style.setProperty('display', 'block')
+    })
+
+canceltBtn.addEventListener('click', e => {
+    popUpGrid.style.setProperty('display', 'none')
+})
+
+// removeBtn.addEventListener('click', e => {
+//     removeBook(e.dataset.removed)??
+   
+// })
+
+addNewBook();
+showBooks();
+
+//functions
+
+// function removeBook(book){
+//     if(item.)
+// }
+
+
+
+function addNewBook() {
+    submitBtn.addEventListener('click', e => {
+        if (bookTitle.value && bookAuthor.value && bookPages.value) {
+            bookRead.checked ? readed = 'readed' : readed = 'unread';
+            const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readed, false)
+            myLibrary.push(newBook);
+            clearAddBookField();
+            e.preventDefault();
+            popUpGrid.style.setProperty('display', 'none')
+            showBooks();
+        }
+    })
+}
+
+function clearAddBookField() {
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
+    readed.value = '';
+}
+
+
+function showBooks() {
+    libraryGrid.innerText='';
+    for (let item of myLibrary) {
+        if(!item.remove){
+        const book = document.createElement('div');
+        const title = document.createElement('p');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+        const read= document.createElement('div');
+        const remove= document.createElement('div');
+        title.innerText = item.title;
+        author.innerText = item.author;
+        pages.innerText = `${item.pages} pages`;
+        read.innerText = item.read;
+        remove.innerText='remove';
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+        book.appendChild(read);
+        book.appendChild(remove);
+        libraryGrid.appendChild(book);
+        book.classList.add('book')
+        remove.classList.add('removeBtn')
+        read.classList.add('readBtn')
+        }
+    }
+}
+
+
+function Book(title, author, pages, read, remove) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.deleted=deleted;
+    this.remove = remove;
 }
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
-function showBooks(){
-    for(let i of myLibrary){
-        
-    }
-}
-
-const newBook =  new Book(bookTitle);
-addBookToLibrary(newBook);
-addBookToLibrary(newBook)
-
-
-console.log(myLibrary);
