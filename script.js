0da3b1ff855ef57dc3bc05a55d26110155939350
Vitor <direcaoass@gmail.com>
2023-01-22 16:15:59 -0300

    remove and read button added

--- script.js
+++ script.js
@@ -7,51 +7,39 @@ const popUpGrid = document.querySelector('.popUp');
 const bookTitle = document.querySelector('.name');
 const bookAuthor = document.querySelector('.author');
 const bookPages = document.querySelector('.pages');
-const bookRead = document.querySelector('.readed');
+const readCheck = document.querySelector('.readCheck');
 const submitBtn = document.querySelector('.submit');
 const canceltBtn = document.querySelector('.cancel');
-const removeBtn=document.querySelector('.removeBtn');
-const readBtn=document.querySelector('readBtn');
+let readStatus;
+let id = 0;
 
-
-let readed;
 
 //events
 
-addBtn.addEventListener('click', e => {
+addBtn.addEventListener('click', () => {
     popUpGrid.style.setProperty('display', 'block')
-    })
-
-canceltBtn.addEventListener('click', e => {
-    popUpGrid.style.setProperty('display', 'none')
 })
 
-// removeBtn.addEventListener('click', e => {
-//     removeBook(e.dataset.removed)??
-   
-// })
+canceltBtn.addEventListener('click', () => {
+    popUpGrid.style.setProperty('display', 'none')
+
+})
 
 addNewBook();
-showBooks();
-
-//functions
-
-// function removeBook(book){
-//     if(item.)
-// }
-
-
 
 function addNewBook() {
     submitBtn.addEventListener('click', e => {
         if (bookTitle.value && bookAuthor.value && bookPages.value) {
-            bookRead.checked ? readed = 'readed' : readed = 'unread';
-            const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readed, false)
+            
+            readCheck.checked ? readStatus = 'readed' : readStatus = 'unread';
+            id++;
+            const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readStatus, id)
             myLibrary.push(newBook);
             clearAddBookField();
             e.preventDefault();
             popUpGrid.style.setProperty('display', 'none')
-            showBooks();
+            createBook(newBook.title, newBook.author, newBook.pages, readStatus, id)
+
         }
     })
 }
@@ -60,43 +48,74 @@ function clearAddBookField() {
     bookTitle.value = '';
     bookAuthor.value = '';
     bookPages.value = '';
-    readed.value = '';
+    readStatus.value = '';
 }
 
+function createBook(title, author, pages, readStatus, id) {
+    const bookElem = document.createElement('div');
+    const titleElem = document.createElement('p');
+    const authorElem = document.createElement('p');
+    const pagesElem = document.createElement('p');
+    const readElem = document.createElement('div');
+    const removeElem = document.createElement('div');
+    titleElem.innerText = title;
+    authorElem.innerText = author;
+    pagesElem.innerText = `${pages} pages`;
+    readElem.innerText = readStatus;
+    removeElem.innerText = 'remove';
+    bookElem.appendChild(titleElem);
+    bookElem.appendChild(authorElem);
+    bookElem.appendChild(pagesElem);
+    bookElem.appendChild(readElem);
+    bookElem.appendChild(removeElem);
+    libraryGrid.appendChild(bookElem);
+    bookElem.classList.add('book');
+    removeElem.classList.add('removeBtn');
+    (readStatus==='readed')?readElem.classList.add('readBtn'):readElem.classList.add('unreadBtn');
+    elementEvents(id, bookElem,readElem)
+}
 
-function showBooks() {
-    libraryGrid.innerText='';
-    for (let item of myLibrary) {
-        if(!item.remove){
-        const book = document.createElement('div');
-        const title = document.createElement('p');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        const read= document.createElement('div');
-        const remove= document.createElement('div');
-        title.innerText = item.title;
-        author.innerText = item.author;
-        pages.innerText = `${item.pages} pages`;
-        read.innerText = item.read;
-        remove.innerText='remove';
-        book.appendChild(title);
-        book.appendChild(author);
-        book.appendChild(pages);
-        book.appendChild(read);
-        book.appendChild(remove);
-        libraryGrid.appendChild(book);
-        book.classList.add('book')
-        remove.classList.add('removeBtn')
-        read.classList.add('readBtn')
-        }
+function elementEvents(id, bookElem,readElem) {
+
+    bookElem.querySelector('.removeBtn').addEventListener('click', () => removeBook(id, bookElem));
+
+    readElem.addEventListener('click', () =>styleReadBtn(readElem))
+        
+}
+
+function styleReadBtn(readElem){
+    if (readElem.innerText === 'unread') {
+        readElem.innerText = 'read';
+        readElem.style.backgroundColor ='green';
     }
+    else {
+        readElem.innerText = 'unread';
+        readElem.style.backgroundColor ='burlywood';
+    }
+
 }
 
 
-function Book(title, author, pages, read, remove) {
+
+function removeBook(id, element) {
+    libraryGrid.removeChild(element)
+    myLibrary = myLibrary.filter(book => book.id != id)
+
+}
+
+
+
+
+
+
+
+
+
+
+function Book(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.remove = remove;
+    this.id = id;
 }
